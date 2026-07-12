229a27e2009a734c7ca26cb1a022999a72956bed
ovenfosten <ovenfosten@gmail.com>
2026-07-12 23:27:19 +0800

    Remove Generic

--- tool.py
+++ tool.py
@@ -1,27 +1,27 @@
-from collections.abc import Callable, Mapping
-from typing import Any, Dict, Generic, TypeVar
+from collections.abc import Callable
 from pydantic import BaseModel
 
 
+class AsterTool:
+    name: str
+    description: str
+    func: Callable[..., object]
+    args_schema: type[BaseModel]
+    return_schema: type[BaseModel]
 
-ArgsT = TypeVar("ArgsT", bound=BaseModel)
-ReturnT = TypeVar("ReturnT", bound=BaseModel)
-class AsterTool(Generic[ArgsT, ReturnT]):
-    def __init__(
-        self,
-        name: str,
-        description: str,
-        func: Callable[[ArgsT], ReturnT],
-        args_schema: type[ArgsT],
-        return_schema: type[ReturnT],
-    ) -> None:
+    def __init__(self,
+                name: str,
+                description: str,
+                func: Callable[..., object],
+                args_schema: type[BaseModel],
+                return_schema: type[BaseModel],) -> None:
         self.name = name
         self.description = description
         self.func = func
         self.args_schema = args_schema
         self.return_schema = return_schema
 
-    def invoke(self, arguments: dict[str, object]) -> ReturnT:
+    def invoke(self, arguments: dict[str, object],) -> BaseModel:
         args = self.args_schema.model_validate(arguments)
         result = self.func(args)
         return self.return_schema.model_validate(result)
