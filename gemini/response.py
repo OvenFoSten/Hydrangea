0ed9d8ab0826730fa44403030ce0bac3225edcd4
ovenfosten <ovenfosten@gmail.com>
2026-07-12 22:57:08 +0800

    Gemini Specific Response Structure.

---
+++ gemini/response.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+from enum import Enum
+
+from google import genai
+from google.genai import types
+    
+
+class GenAIAgentResponse():
+    _content:types.Content
+    
+    
+    thoughts:list[str]|None
+    tool_calls:list[types.FunctionCall]|None
+    output:str
+    
+    
+    def __init__(self, content:types.Content)->None:
+        self._content = content
+        self._thoughts = None
+        
+        self.output = ""
+        self.tool_calls = None
+        if not content.parts:
+            raise ValueError("No content from Google, please check the API availability.")
+        
+        response_thoughts:list[str] = []
+        response_tool_calls:list[types.FunctionCall] = []
+        for part in content.parts:
+            if part.thought and part.text:
+                response_thoughts.append(part.text)
+                continue
+
+            if part.function_call:
+                response_tool_calls.append(part.function_call)
+                continue
+
+            if part.text:
+                self.output += part.text
+                continue
+
+                
+        if response_thoughts:
+            self._thoughts = response_thoughts
+        if response_tool_calls:
+            self.tool_calls = response_tool_calls
+
