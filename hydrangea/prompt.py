38f5f5ba69f62454a79722d96572199ed4940b27
ovenfosten <ovenfosten@gmail.com>
2026-08-27 19:07:39 +0800

    Remove unnecessary prompt class for now. Remove implement of "prompt slots". Refs #1

--- hydrangea/prompt.py
+++ hydrangea/prompt.py
@@ -1,9 +1,6 @@
-class Prompt:
-    _insturction: str
+from typing import NewType
 
-    def __init__(self, instruction:str)->None:
-        self._insturction = instruction
-        
-    def render(self, slots:dict[str, object])->str:
-        slots_formatted = "\n".join([f"{key}: {value}" for key, value in slots.items()])
-        return f"{self._insturction}\n{slots_formatted}"
+SystemInstruction = NewType(
+    "SystemInstruction",
+    str,
+)
